b0fe36202d29a7e75ca4b4198ac75ecf72e8a270
Matheus Barbosa <mattbarb93@gmail.com>
2020-04-23 12:04:35 -0400

    added logic to create the new URL. The new URL will be displayed, and then automatically selected, so the user can easily copy iy

--- index.js
+++ index.js
@@ -3,13 +3,27 @@ document.querySelector('form').addEventListener('submit', event => {
 
     //Setting variable for the user's original input
     const input = document.querySelector
-    ('#message-input');
+        ('#message-input');
 
     //Input for the Base64 Conversion function
     const encrypted = btoa(input.value)
 
     //Setting the shareable link equal to the message link after conversion
-    document.querySelector('#link-input').value = encrypted
 
+    /*Now, we'll update the code to generate the new URL. 
     
+    Window.location allows us to get the current URL of the user (by putting it under `${window.location}`)
+    
+    Call that, and the encrypted message
+
+    */
+
+    const linkInput = document.querySelector('#link-input');
+    
+    linkInput.value = `${window.location}#${encrypted}`;
+
+    //new URL is instantly selected, so the user can just copy
+    linkInput.select();
+
+
 })
